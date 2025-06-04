f6877222f2399e3cdeb1fc8ecc82461144539ce0
Santiago Sabio <138256848+SantiSabio@users.noreply.github.com>
2025-06-04 13:56:08 -0300

    refactor: update admin registration for AuthUser and Persona models remove unused view functions for creating, deleting, and modifying turnos

--- app/backend/authentification/admin.py
+++ app/backend/authentification/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import AuthUser, Persona
 
-# Register your models here.
+@admin.register(AuthUser)
+class AuthUserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')
+    search_fields = ('username',)
+
+@admin.register(Persona)
+class PersonaAdmin(admin.ModelAdmin):
+    list_display = (
+        'auth_user', 'tipo_usuario', 'nombre', 'apellido',
+        'fecha_nacimiento', 'especialidad', 'consultorio'
+    )
+    search_fields = (
+        'nombre', 'apellido', 'tipo_usuario', 'auth_user__username', 'especialidad', 'consultorio'
+    )
+    list_filter = ('tipo_usuario', 'especialidad', 'consultorio')
