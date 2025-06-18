fae9b8fc9a535da8d00a538a479e23d112e7bfe2
Jeremias Ocaña <jere.oca@hotmail.com>
2025-06-17 23:16:13 -0300

    Update urls.py

--- app/backend/dashboards/urls.py
+++ app/backend/dashboards/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard_redirect, name='dashboard_redirect'),
+    path('', views.api_dashboard, name='dashboard_redirect'),
     path('doctor/', views.doctor_dashboard, name='doctor_dashboard'),
     path('paciente/', views.paciente_dashboard, name='paciente_dashboard'),
-    path('administrativo/', views.admin_dashboard, name='admin_dashboard'),
-] 
+    path('secretario/', views.secretario_dashboard, name='secretario_dashboard'),
+]
